988404e9438fd9bca3e0bd809650c9c77284a336
Amir Kantor <amir.kantor@gmail.com>
2024-01-03 08:38:14 +0200

    move root and add constants to init file

--- ak_tools/__init__.py
+++ ak_tools/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.1.0"
+from pathlib import Path
+
+import toml
+
+ROOT = Path(__file__).parent.parent
+PYPROJECT = toml.load(ROOT / "pyproject.toml")
+__version__ = PYPROJECT["tool"]["poetry"]["version"]
+PROJECT_NAME = PYPROJECT["tool"]["poetry"]["name"]
--- ak_tools/main.py
+++ ak_tools/main.py
@@ -1,11 +1,9 @@
 import sys
 
-import toml
 import typer
 from rich import print
 
-from . import __version__ as package_version
-from .root import ROOT
+from . import PROJECT_NAME, __version__
 
 app = typer.Typer()
 
@@ -15,7 +13,7 @@ def command1():
     """Command #1"""
     print("Command #1 started ...")
     print("system", sys.version)
-    print("package", package_version)
+    print("package", __version__)
     print("... command #1 ended")
 
 
@@ -24,9 +22,9 @@ def command2():
     """Command #2"""
     print("Command #2 started ...")
     print("system", sys.version)
-    print("package", package_version)
+    print("package", __version__)
     print("... command #2 ended")
 
 
 def main():
-    app(prog_name=toml.load(ROOT / "pyproject.toml")["tool"]["poetry"]["name"])
+    app(prog_name=PROJECT_NAME)
